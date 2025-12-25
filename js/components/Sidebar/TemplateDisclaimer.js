ba33b6d8ccb165a87bce2f9d57a030af4a33ba13
Duckly <76588336+FudezPal@users.noreply.github.com>
2025-12-25 23:40:21 +0200

    Update TemplateDisclaimer.js

--- js/components/Sidebar/TemplateDisclaimer.js
+++ js/components/Sidebar/TemplateDisclaimer.js
@@ -1,7 +1,7 @@
 export default {
     template: `
         <div class="og">
-            <p class="type-label-md">Some of website template from <a class="director" href="https://tsl.pages.dev/" target="_blank"> The Shitty List </a>; and <a class="director" href="https://laylist.pages.dev/#/></a> The Layout List </a>.</p>
+            <p class="type-label-md">Some of website template from <a class="director" href="https://tsl.pages.dev/" target="_blank"> The Shitty List </a>; and <a class="director" href="https://laylist.pages.dev/#/" target="_blank"> The Layout List </a>.</p>
         </div>
     `
 }
