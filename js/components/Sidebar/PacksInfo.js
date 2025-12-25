9e8cb8b7de52c6b3ce9ffba3a8820898e3065869
Duckly <76588336+FudezPal@users.noreply.github.com>
2025-12-26 01:44:28 +0200

    Update PacksInfo.js

--- js/components/Sidebar/PacksInfo.js
+++ js/components/Sidebar/PacksInfo.js
@@ -2,7 +2,7 @@ export default {
     template: `
         <h3>About Packs</h3>
         <div class="right-text">
-            <p>Packs are sets of levels on the Layout List chosen by the staff team that share distinct commonalities and are within a close difficulty range (generally ± 1 difficulty tier, with the exception of very specific and/or joke packs).</p>
+            <p>Packs are sets of levels chosen by the staff team that share distinct commonalities and are within a close difficulty range (generally ± 1 difficulty tier, with the exception of very specific and/or joke packs).</p>
             <p>If you have a suggestion for a new pack, feel free to share it with the list team in our Discord server!</p>
             <p>If you beat all the levels in a pack, it gets displayed on your profile in the leaderboard!</p>
         </div>
