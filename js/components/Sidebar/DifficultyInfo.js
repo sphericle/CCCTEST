fec17dfad3b218e2041e43be357ad5e5a8001fb7
Duckly <realactethx@gmail.com>
2025-12-26 03:34:07 +0200

    Update DifficultyInfo.js

--- js/components/Sidebar/DifficultyInfo.js
+++ js/components/Sidebar/DifficultyInfo.js
@@ -1,34 +1,4 @@
 export default {
     template: `
-        <h3>Difficulty Rankings</h3>
-        <div class="right-text">
-            <p>
-                Silent Challenge = Hardest Challenges on the list. (401 to 750 Points)
-            </p>
-            <p>
-                Legendary Challenge = LEGENDARY Challenges. (201 to 400 Points)
-            </p>
-            <p>
-                Extreme Challenge = EXTREME Challenges (101 to 200 Points)
-            </p>
-            <p>
-                Mythical Challenge = MYTHICAL Challenges (71 to 100 Points)
-            </p>
-            <p>
-                Insane Challenge = INSANE Challenges (41 to 70 Points)
-            </p>
-            <p>
-                Hard Challenge = HARD Challenges (21 to 40 Points)
-            </p>
-            <p>
-                Medium Challenge = MEDIUM Challenges (11 to 20 Points)
-            </p>
-            <p>
-                Easy Challenge = EASY Challenges (6 to 10 Points)
-            </p>
-            <p>
-                Beginner Challenge = BEGINNER Challenges (1 to 5 Points)
-            </p>
-        </div>
-    `
+      
 }
