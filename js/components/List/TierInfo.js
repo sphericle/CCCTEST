7d105eab36aa19feb4f142300253969470f4af9e
sphericle <86196223+sphericle@users.noreply.github.com>
2025-12-25 22:47:55 -0500

    fix: handle tiers min/max with no enjoyments

--- js/components/List/TierInfo.js
+++ js/components/List/TierInfo.js
@@ -22,10 +22,10 @@ export default {
             <h2 style="padding-top:1rem"># of levels in tier: {{ fetchTierLength(list, level.difficulty) }}</h2>
             <h2 style="padding-bottom:1rem">Points in tier: {{ localize(fetchTotalScore(list, level.difficulty)) }}</h2>
             <tr style="justify-content: center; align-items: center;">
-                <td><h3 class="tier-info">Highest enjoyment: {{ fetchHighestEnjoyment(list, level.difficulty) }}</h3></td>
+                <td><h3 class="tier-info">Highest enjoyment: {{ fetchHighestEnjoyment(list, level.difficulty) || "N/A" }}</h3></td>
             </tr>
             <tr style="justify-content: center; align-items: center;">
-                <td><h3 class="tier-info" style="padding-bottom:0.5rem">Lowest enjoyment: {{ fetchLowestEnjoyment(list, level.difficulty) }}</h3></td>
+                <td><h3 class="tier-info" style="padding-bottom:0.5rem">Lowest enjoyment: {{ fetchLowestEnjoyment(list, level.difficulty) || "N/A" }}</h3></td>
             </tr>
             <p style="padding-top:1.5rem">The levels {{ descending ? 'below' : 'above' }} are {{ ["Beginner", "Easy", "Medium", "Hard", "Insane", "Mythical", "Extreme", "Supreme", "Ethereal", "Legendary", "Silent", "Impossible"][level.difficulty] }} Challenges.</p>
 
